5bc8ad1fd289999a93f195ce13ff13881c142e4a
PiratePowWow <Thi3v3ry>
2016-02-10 14:38:05 -0500

    2.3 Assignment Complete

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 /**
@@ -5,37 +6,32 @@ import java.util.Scanner;
  */
 public class Main {
     static Scanner scanner = new Scanner(System.in);
+    public static Movie movie = new Movie();
 
+    public static void main(String[] args) throws IOException {
+        System.out.println("Welcome to The Force Awakens Movie stats. Here are your stats:");
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to The Force Awakens Movie stats");
-
-        try {
-            Movie.loadMovie();
-            listOptions();
+        while (true) {
+            try {
+                movie = Movie.loadMovie();
+                Movie.print(movie);
+                update();
+            } catch (Exception e) {
+                System.out.println("No saved information found, generating new movie stat file:");
+                movie = Movie.createMovie();
+            }
+            Movie.saveMovie(movie);
         }
-        catch (Exception e) {
-            System.out.println("No saved information found, generating new movie stat file:");
-        }
-
-
-
-
-
     }
-
-    public static void listOptions(){
+    public static void update(){
+        System.out.println("Would you like to update your movie stats?");
         String selection = scanner.nextLine();
-        switch (selection){
-            case "1":
-                break;
-            case "2":
-                break;
-            default:
-                break;
-
+        if (selection.equalsIgnoreCase("y")){
+            movie = Movie.createMovie();
         }
-
+        else{
+            System.exit(0);
+        }
     }
 
 }
--- src/Movie.java
+++ src/Movie.java
@@ -1,3 +1,12 @@
+import jodd.json.JsonParser;
+import jodd.json.JsonSerializer;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * Created by PiratePowWow on 2/10/16.
  */
@@ -8,12 +17,59 @@ public class Movie {
     private boolean isPartofSeries;
     private boolean wouldRecommend;
 
-    public static void saveMovie() {
-
+    public Movie() {
     }
 
-    public static void loadMovie() {
+    public Movie(String title, int stars, String genre, boolean isPartofSeries, boolean wouldRecommend) {
+        this.title = title;
+        this.stars = stars;
+        this.genre = genre;
+        this.isPartofSeries = isPartofSeries;
+        this.wouldRecommend = wouldRecommend;
+    }
 
+    public static Movie createMovie() {
+
+        System.out.println("What is the title of the movie?");
+        String title = Main.scanner.nextLine();
+        System.out.println("How many stars would you rate this movie? [0-5]");
+        int stars = Integer.valueOf(Main.scanner.nextLine());
+        System.out.println("What is the genre of the movie?");
+        String genre = Main.scanner.nextLine();
+        System.out.println("Is this movie part of a series of movies?");
+        boolean isPartOfSeries = Boolean.valueOf(Main.scanner.nextLine());
+        System.out.println("Would you recommend this movie to others?");
+        boolean wouldRecommend = Boolean.valueOf(Main.scanner.nextLine());
+        return new Movie(title, stars, genre, isPartOfSeries, wouldRecommend);
+    }
+
+    public static void saveMovie(Movie movie) throws IOException {
+        JsonSerializer serializer = new JsonSerializer();
+        String json = serializer.include("*").serialize(movie);
+        File savemovie = new File("savemovie.json");
+        FileWriter fw = new FileWriter(savemovie);
+        fw.write(json);
+        fw.close();
+        System.out.println("Movie stats saved.");
+        System.out.println("Here are your movie stats:");
+    }
+
+    public static Movie loadMovie() throws FileNotFoundException {
+        File f = new File("savemovie.json");
+        Scanner s = new Scanner(f);
+        s.useDelimiter("\\Z");
+        String contents = s.next();
+
+        JsonParser movie = new JsonParser();
+        return movie.parse(contents, Movie.class);
+    }
+
+    public static void print(Movie movie){
+        System.out.printf("Title: %s\n", movie.title);
+        System.out.printf("Rating: %d Stars\n", movie.stars);
+        System.out.printf("Genre: %s\n", movie.genre);
+        System.out.printf("Part of a Series?: %b\n", movie.isPartofSeries);
+        System.out.printf("Would you Recommend this Movie?: %b\n", movie.wouldRecommend);
     }
 
     public void setTitle(String title) {
@@ -34,5 +90,25 @@ public class Movie {
 
     public void setWouldRecommend(boolean wouldRecommend) {
         this.wouldRecommend = wouldRecommend;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getStars() {
+        return stars;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public boolean isPartofSeries() {
+        return isPartofSeries;
+    }
+
+    public boolean isWouldRecommend() {
+        return wouldRecommend;
     }
 }
